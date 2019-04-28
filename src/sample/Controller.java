6f2a4c311abaabb50987c181530d693fbc8c3b3c
Kamil Rączka <raczka.kamil@onet.pl>
2019-04-28 21:24:57 +0200

    'Nowy'

---
+++ src/sample/Controller.java
@@ -0,0 +1,55 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    ComboBox Perfuma_Rodzaj;
+    @FXML
+    ComboBox Lakier_Rodzaj;
+    @FXML
+    ComboBox Lakier_Kolor;
+    @FXML
+    ComboBox Krem_Rodzaj;
+    @FXML
+    ComboBox Krem_Wiek;
+    @FXML
+    ComboBox Tusz_Rodzaj;
+    @FXML
+    Button Perfuma_Akceptacja;
+    @FXML
+    Button Lakier_Akceptacja;
+    @FXML
+    Button Krem_Akceptacja;
+    @FXML
+    Button Tusz_Akceptacja;
+    @FXML
+    TextField Perfuma_Ilosc;
+    @FXML
+    TextField Lakier_Ilosc;
+    @FXML
+    TextField Krem_Ilosc;
+    @FXML
+    TextField Tusz_Ilosc;
+
+
+    public void P_AKCEPTACJA()
+    {
+
+    }
+
+    public void L_AKCEPTACJA()
+    {
+
+    }
+    public void K_AKCEPTACJA(){
+
+    }
+
+    public void T_AKCEPTACJA() {
+
+    }
+}
